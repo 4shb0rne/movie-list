fbbb8483b73b27b0c1d3c82878f31fb4f4b92258
Inigo Hersanta Sutandyo <86969846+inigoSutandyo@users.noreply.github.com>
2022-12-29 11:53:27 +0700

    actor profile info in detail

--- resources/views/actors/detail.blade.php
+++ resources/views/actors/detail.blade.php
@@ -4,35 +4,50 @@
 @endsection
 @section('content')
     <div class="container">
-        <div class="my-3 mx-5 d-flex justify-content-center row">
-            <div class="col-4 mx-3 d-flex justify-content-end">
+        <div class="my-3 mx-5 d-flex flex-row justify-content-center">
+            <div class="mx-3 d-flex flex-column">
                 <div class="actor-detail-image" style="
                     background: url({{ asset('storage/actors/'.$actor->image_url) }})
                         , no-repeat;
                     background-size: cover;
                     background-position: center;
                 ">
-                    <div class="d-flex flex-column p-1">
-                        <div>
-                            <a href="{{ route('edit-actor', ['actor'=>$actor->id]) }}">
-                                <i class="fa-solid fa-pen-to-square me-2 fs-5 background-red circle-icon"></i>
-                            </a>
-                        </div>
-                        <form action="{{ route('delete-actor', ['actor'=>$actor->id]) }}" method="POST">
-                            @csrf
-                            @method('delete')
-                            <button type="submit"
-                                style="background: none;
-                                padding: 0;
-                                border:none"
-                            >
-                                <i class="fa-solid fa-trash fs-5 background-red circle-icon"></i>
-                            </button>
-                        </form>
-                    </div>
+                    @auth
+                        @if(auth()->user()->isAdmin())
+                            <div class="d-flex flex-column p-1">
+                                <div>
+                                    <a href="{{ route('edit-actor', ['actor'=>$actor->id]) }}">
+                                        <i class="fa-solid fa-pen-to-square me-2 fs-5 background-red circle-icon"></i>
+                                    </a>
+                                </div>
+                                <form action="{{ route('delete-actor', ['actor'=>$actor->id]) }}" method="POST">
+                                    @csrf
+                                    @method('delete')
+                                    <button type="submit"
+                                        style="background: none;
+                                        padding: 0;
+                                        border:none"
+                                    >
+                                        <i class="fa-solid fa-trash fs-5 background-red circle-icon"></i>
+                                    </button>
+                                </form>
+                            </div>
+                        @endif
+                    @endauth
+                </div>
+                <div class="d-flex flex-column align-items-start">
+                    <p class="fs-3 fw-bold mb-1">Personal Info</p>
+                    <span class="fs-5 mb-1">Popularity</span>
+                    <span class="text-muted mb-1">{{$actor->popularity}}</span>
+                    <span class="fs-5 mb-1">Gender</span>
+                    <span class="text-muted mb-1">{{$actor->gender}}</span>
+                    <span class="fs-5 mb-1">Birthday</span>
+                    <span class="text-muted mb-1">{{$actor->dob}}</span>
+                    <span class="fs-5 mb-1">Place of Birth</span>
+                    <span class="text-muted mb-1">{{$actor->place_of_birth}}</span>
                 </div>
             </div>
-            <div class="col-6">
+            <div class="w-50 d-flex flex-column">
                 <div>
                     <h2>{{$actor->name}}</h2>
                     <h4 class="fw-normal">Biography</h4>
